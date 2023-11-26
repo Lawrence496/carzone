2444c435b9b669f3085d8ccd3890e368f5d0c2e2
Lawrence Anyalechi <58028325+Lawrence496@users.noreply.github.com>
2023-11-26 22:24:17 +0000

    Update deployment.py

--- carzone/deployment.py
+++ carzone/deployment.py
@@ -2,6 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
+WEBSITE_HOSTNAME = "https://carzone.azurewebsites.net/"
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 
