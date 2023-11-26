620883911ac936fd892574ef513522fb85bbceae
Lawrence Anyalechi <58028325+Lawrence496@users.noreply.github.com>
2023-11-26 21:24:21 +0000

    Update wsgi.py

--- carzone/wsgi.py
+++ carzone/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 settings_module = 'carzone.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'carzone.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carzone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
