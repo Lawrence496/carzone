103c9c3dd6872e67856fe8977640b26a2224d8ec
Lawrence Anyalechi <58028325+Lawrence496@users.noreply.github.com>
2023-11-26 22:13:35 +0000

    Update deployment.py

--- carzone/deployment.py
+++ carzone/deployment.py
@@ -4,8 +4,8 @@ from .settings import BASE_DIR
 
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ['carzone.azurewebsites.net']]
+CSRF_TRUSTED_ORIGINS = ['https://carzone.azurewebsites.net']
 DEBUG = False
 
 # WhiteNoise configuration
